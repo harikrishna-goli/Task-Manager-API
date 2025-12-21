b8d295a860451478371019cd8c2fc1498721ea83
Hari Krishna Goli <harikrishna.goli@hotmail.com>
2025-12-21 22:22:39 +0530

    UserLogin

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app import schemas, models
-from app.security import hash_password
+from app.security import hash_password, create_access_token
 from app.database import get_db
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
@@ -20,7 +20,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
                   .first())
     
     if userexists:
-        raise HTTPException(status_code=400, 
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                             detail="Username already registered")
     
     hashed_password = hash_password(user.password)
@@ -33,7 +33,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 @router.post("/login/")
-def login(user_credentials: schemas.UserLogin):
-    # TODO: Add authentication logic
-    return {"message": "User logged in"}
+def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+    user = (db.query(models.User)
+            .filter(models.User.username == user_credentials.username)
+            .first())
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid username or password")
+    access_token = create_access_token(data=user.username)
+    return {"access_token": access_token,
+            "token_type": "bearer"}
 
